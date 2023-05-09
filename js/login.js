a91319a33e98661075d6f927d8bf0454ff6dc9e9
chaewon <juh7814@gmail.com>
2023-05-09 16:47:43 +0900

    💄 : "로그인창 UI 수정"

--- js/login.js
+++ js/login.js
@@ -1,10 +1,32 @@
+const login = document.querySelector(".login_form")
+const loginBack = document.querySelector(".login_back")
+const loginId = login.querySelector("#login_id")
+const loginBtn = login.querySelector("button")
 const userName = document.querySelector(".user_name")
 const header = document.querySelector('.headline') 
 const NAME_KEY = "name";
+const HIDDEN_CLASS = "hidden";
 
-    const nameValue = prompt("이름을 입력하세요");
+function loginHandler (event) {
+    event.preventDefault();
+    login.classList.add(HIDDEN_CLASS)
+    loginBack.classList.add(HIDDEN_CLASS)
+    const nameValue = loginId.value;
     localStorage.setItem(NAME_KEY,nameValue);
-    const getName = localStorage.getItem(NAME_KEY);
-    userName.innerText = getName
-    header.innerText = `${getName}의 미니홈피 입니다.`
+    paintHandler(nameValue)
+}
 
+function paintHandler (nameValue) {
+    userName.innerText = nameValue
+    header.innerText = `${nameValue}의 미니홈피 입니다.`
+}
+
+const savedName = localStorage.getItem(NAME_KEY);
+
+if(savedName === null) {
+    login.classList.remove(HIDDEN_CLASS)
+    loginBack.classList.remove(HIDDEN_CLASS)
+    loginBtn.addEventListener("click", loginHandler )
+} else {
+    paintHandler(savedName)
+}
